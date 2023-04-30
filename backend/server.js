5aa794522eaf4ed6baac7bedaca35710dbf6258e
Pabasaraa <pabasara.was@gmail.com>
2023-04-30 14:10:17 +0530

    Initial commit. Create product-management backend and create frontend file structure

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { connectDB } from "./database.js";
+import dotenv from "dotenv";
+
+// Import routes
+import productRoutes from "./product-management-service/routes/product.route.js";
+
+// Load environment variables from .env file to the process.env object
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use("/", (res) => {
+  res.send("EcoHub backend service!");
+});
+
+// Use routes
+app.use("/products", productRoutes);
+
+// Connect to the database
+connectDB();
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
