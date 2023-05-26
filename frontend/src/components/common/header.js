dab0008799c48e2925435bdf74b484814e170f61
Navodi P.T <101013516+IT20639662@users.noreply.github.com>
2023-05-26 20:53:36 +0530

    feat: :fire: test cases

--- frontend/src/components/common/header.js
+++ frontend/src/components/common/header.js
@@ -47,7 +47,7 @@ function NavBar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"#"}>
+              <Link className="nav-link" to={"/contactForm"}>
                 <b>Contact Us</b>
               </Link>
             </li>
